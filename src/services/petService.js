b4b7e2c6abdd4084af380fd88679b883f956fae2
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-08-20 01:42:01 +0900

    feat: pet api 개발 추가

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,4 +1,20 @@
 import { PetModel } from "../db/models/index.js";
 
-class PetService {}
+class PetService {
+    constructor() {
+        this.petModel = new PetModel();
+    }
+    async getPet(id) {
+        return await this.petModel.find(id);
+    }
+    async addPet(pet) {
+        return await this.petModel.create(pet);
+    }
+    async updatePet(id, pet) {
+        return await this.petModel.update(id, pet);
+    }
+    async deletePet(id) {
+        return await this.petModel.delete(id);
+    }
+}
 export default PetService;
