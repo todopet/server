3289c18639f75087c724482602f3e117afcea9e0
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-08-23 12:36:35 +0900

    feat: todo 완료 시 히스토리 저장, 보상 추가

--- src/db/models/historyModel.js
+++ src/db/models/historyModel.js
@@ -6,5 +6,8 @@ class HistoryModel {
     async findHistory(userId, contentId) {
         return await History.find({ userId, contentId }).lean();
     }
+    async createHistory(userId, contentId) {
+        return (await History.create({ userId, contentId})).toObject();
+    }
 }
 export default HistoryModel;
