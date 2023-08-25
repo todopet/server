7ba99ac04c7b21ce7041d4cf58eed4402a8ec408
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-08-25 12:24:58 +0900

    fix: 상위 10개 데이터만 불러오기, 투두 완료 수 같을 시 최근 생성일자 기준 정렬

--- src/db/models/historyModel.js
+++ src/db/models/historyModel.js
@@ -33,7 +33,7 @@ class HistoryModel {
                 }
             },
             {
-                $sort: { count: -1 } // 갯수에 따라 내림차순 정렬 (랭킹 순서)
+                $sort: { count: -1, createdAt: -1 } // 갯수에 따라 내림차순 정렬 (랭킹 순서), 갯수 같을 시 최근 생성 순서
             },
             {
                 $project: {
@@ -41,6 +41,9 @@ class HistoryModel {
                     userId: '$_id', // 결과에서 필요한 필드만 남김
                     count: 1 // count 필드
                 }
+            },
+            {
+                $limit: 10 // 상위 10개 데이터만 가져오기
             }
         ]);
     }
