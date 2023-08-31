0bf79e784ff3e43f31cd23ac1aff34fc56aee019
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-09-01 03:41:32 +0900

    fix: 누락사항 추가

--- src/middlewares/signatureMiddleware.js
+++ src/middlewares/signatureMiddleware.js
@@ -1,14 +1,13 @@
 import { buildResponse } from '../misc/utils.js';
 
 const signatureMiddleware = (req, res, next) => {
-  const receivedTime = req.headers['x-custom-Data'];
+  const receivedTime = req.headers['x-custom-data'];
   const checkReceivedTime = (receivedTime - 1000) / 4;
   const maxTimeDifference = 60 * 1000; // 1분 (밀리초 단위)
   const currentTime = Date.now();
-
   const timeDifference = currentTime - checkReceivedTime;
-
   if (timeDifference <= maxTimeDifference) {
+    next();
   } else {
     return res.json(
       buildResponse({
