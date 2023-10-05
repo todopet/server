79ab0421d0e6c8b2f0f03dfd4e147a86c2518843
원종석 <63568239+rebi13@users.noreply.github.com>
2023-10-05 17:01:06 +0900

    hotfix: cors 설정 변경3

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ const corsOptions = {
     credentials: true
 };
 //CORS 에러방지
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://todopet.vercel.app'); // 클라이언트 도메인
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH'); // 필요한 HTTP 메서드 추가
-    res.header('Access-Control-Allow-Credentials', 'true'); // credentials 허용
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'https://todopet.vercel.app'); // 클라이언트 도메인
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH'); // 필요한 HTTP 메서드 추가
+//     res.header('Access-Control-Allow-Credentials', 'true'); // credentials 허용
+// });
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
