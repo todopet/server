caae88abf2dd1f3b314782dace81129a91de9a92
rebi13 <rebi13@naver.com>
2026-02-28 15:23:22 +0900

    fix: 회원탈퇴 시 잘못된 토큰 쿠키 설정 제거 (refs #45)

--- src/services/userService.js
+++ src/services/userService.js
@@ -125,18 +125,6 @@ class UserService {
     }
   }
 
-  async WithdrawUserAndToken(userId) {
-    try {
-      // 회원 탈퇴 처리와 새로운 토큰 발급 수행
-      const newToken = await this.withdrawUser(userId);
-
-      return newToken; // 새로운 토큰 반환
-    } catch (error) {
-      console.error('Error withdrawing user:', error);
-      throw error; // 에러 다시 throw
-    }
-  }
-
   async withdrawUser(userId) {
     const updateResult = await this.userModel.updateMembershipStatus(
       userId,
